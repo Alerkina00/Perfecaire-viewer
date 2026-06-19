7bb9b38e0b42c5ce82410239571c98bdcacf19a7
THAIS <86262853+Alerkina00@users.noreply.github.com>
2026-06-19 13:05:42 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -16,10 +16,8 @@ app.use(cors());
 app.use(express.json());
 
 // ← WASM antes do static, senão o 404 do static vence
-app.get('/web-ifc.wasm', (req, res) => {
-  res.sendFile(
-    path.join(__dirname, '../node_modules/web-ifc/web-ifc.wasm')
-  );
+app.get('/api/wasm/web-ifc.wasm', (req, res) => {
+  res.sendFile(path.join(__dirname, '../node_modules/web-ifc/web-ifc.wasm'));
 });
 
 // Arquivos estáticos do viewer
